8070e01b951e7db7005908a4f7310784904a6c5f
douwehorsthuis <douwehorsthuis@gmail.com>
2021-09-22 15:00:34 -0400

    updating

--- README.md
+++ README.md
@@ -1,7 +1,111 @@
-# ICA_Practice
-A script to practice what IC components to delete 
 
-Work in progress
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
 
 
-Will explain here what the script does and why it's usefull 
+<br />
+<p align="center">
+  <a href="https://github.com/DouweHorsthuis/ICA_Practice/">
+       <img src="images/logo.jpeg" alt="Logo" width="286" height="120">
+  </a> 
+
+<h3 align="center">Practice for deleting IC components</h3>
+
+<h4 align="center">This project is build so that people can practice what components they should delete after doing an Independent Component Analysis (ICA).  It uses EEGLAB and it's IClabel toolbox. </h4>
+
+
+**Table of Contents**
+  
+1. [About the project](#about-the-project)
+    - [Built With](#built-with)
+3. [Manual vs. IClabel](#manual-vs.-iclabel)
+3. [Contributing](#contributing)
+3. [License](#license)
+3. [Contact](#contact)
+3. [Acknowledgement](#acknowledgement)
+3. [Usage](#usage)
+
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+[![ICA Video](https://youtu.be/kWAjhXr7pT4?list=PLXc9qfVbMMN2uDadxZ_OEsHjzcRtlLNxc)](https://youtu.be/kWAjhXr7pT4?list=PLXc9qfVbMMN2uDadxZ_OEsHjzcRtlLNxc)
+
+
+
+
+### Built With
+
+* [EEGlab](https://github.com/sccn/eeglab)
+* [IClabel](https://github.com/sccn/ICLabel)
+
+
+
+<!-- ROADMAP -->
+## Manual vs. IClabel
+
+When delete components manually this is what people focused on:
+
+
+
+IClabel is build using the input of a lot of users to train the program. This means that it's also based on the input of less expert humans. 
+
+Similarities: when we tested it in our lab we noticed that for eye components if we set the threshold to >70% eye and <10% brain the toolbox would exclude the same amount of eyecomponents as we would. 
+Differences: when we tested what other components to delete (we decided to not delete "other" and "brain") it became impossible for anyone to delete the same components as the toolbox would. 
+
+Potential conclusion: Eye components are well spotted by both experienced people in our lab as well as the toolbox. However when it comes to other components this becomes less the case.
+
+
+
+<!-- CONTRIBUTING -->
+## IC components explained
+
+Put here the eyecomponents
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Douwe Horsthuis - douwehorsthuis@gmail.com
+Project Link: [https://github.com/DouweHorsthuis/ICA_Practice/](https://github.com/DouweHorsthuis/ICA_Practice/)
+
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Made possible by
+
+* [Ana Francisco](https://github.com/anafrancisco)
+* [Douwe Horsthuis](https://github.com/DouweHorsthuis)
+* [Filip De Sanctis](https://github.com/pdesanctis)
+* Sophie Molholm
+* [Seyda Tikir](https://github.com/tikirs)
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/DouweHorsthuis/ICA_Practice.svg?style=for-the-badge
+[contributors-url]: https://github.com/DouweHorsthuis/ICA_Practice/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/DouweHorsthuis/ICA_Practice.svg?style=for-the-badge
+[forks-url]: https://github.com/DouweHorsthuis/ICA_Practice/network/members
+[stars-shield]: https://img.shields.io/github/stars/DouweHorsthuis/ICA_Practice.svg?style=for-the-badge
+[stars-url]: https://github.com/DouweHorsthuis/ICA_Practice/stargazers
+[issues-shield]: https://img.shields.io/github/issues/DouweHorsthuis/ICA_Practice.svg?style=for-the-badge
+[issues-url]: https://github.com/DouweHorsthuis/ICA_Practice/issues
+[license-shield]: https://img.shields.io/github/license/DouweHorsthuis/ICA_Practice.svg?style=for-the-badge
+[license-url]: https://github.com/DouweHorsthuis/ICA_Practice/blob/master/LICENSE.txt
+
--- src/Deleting_IC_practice.m
+++ src/Deleting_IC_practice.m
@@ -1,6 +1,6 @@
 % This script is created to practice deleting IC components after doing ICA.
 % It plots the scalpmaps of the manual delete components as well as the auto deleted ones.
-% Threshold for auto deletion of components decided after comparing how ICA experts 
+% Threshold for auto deletion of components decided after comparing how ICA experts
 % in our lab have been doing this for years manually.
 % Deleting IC components requires a lot of knowledge and practice and has a huge impact on the data.
 % For explanation about why a components is bad, or what to look for, consult with an expert.
@@ -14,12 +14,17 @@ clear variables
 eeglab
 home_path  = '\\data.einsteinmed.org\users\CNL Lab\Analysis\ICA_test\Data\'; %where the data is stored
 subject_list = {'2201' '2204' '2207' '2208' '7003' '7007' '7073' '7074' '7808' '7123' '10092' '10130' '10476' '10590' '10748' '12005' '12449' '12482' '12648'};
-save_path = '\\data.einsteinmed.org\users\CNL Lab\Analysis\ICA_test\Practice\'; %where to save the figures
+save_path = 'C:\Users\dohorsth\Documents\GitHub\ICA_Practice\testing\'; %where to save the figures
 eye_percentage= 0.70; %what is the minimum size for the eye component's eye (70% is what matched with the experts in our lab)
-components =num2cell(zeros(length(subject_list),3)); 
+components =num2cell(zeros(length(subject_list),3));
 auto_components.id=num2cell(zeros(length(subject_list),1));
 auto_components.eyecomponents=num2cell(zeros(1,length(subject_list)));
 auto_components.other_no_eye= num2cell(zeros(1,length(subject_list)));
+prompt = 'Do you want to look at eye components only [y/n]?';
+type = input(prompt, 's');
+if strcmp(type, 'n')
+    type = 'N';
+end
 
 for s=1:length(subject_list)
     data_path  = [home_path subject_list{s} ''];% Path to the folder containing the current subject's data
@@ -37,45 +42,50 @@ for s=1:length(subject_list)
     %Promting what components to delete
     prompt = 'Eye components? If yes, input the numbers in [ ]. for example [1 2 10] ';
     eye_components = input(prompt);
-    prompt = 'Are there other components you would delete? If yes, input the numbers in [ ]. for example [1 2 10] ';
-    other_components = input(prompt);
-    %% noise except eye part
-    %is going to create a topoplot and save it
-    if isempty(other_components)
-        figure;title(subject_list{s}); text( 0.2,0.5, 'there are no "other" components manually selected')
-        other_components = 0;
-    elseif length(other_components) == 1
-        figure;pop_topoplot(EEG ,0, other_components, subject_list{s},[1 1] ,0,'electrodes','on');
-    else
-        pop_topoplot(EEG, 0, other_components ,subject_list{s},[ceil(sqrt(length(other_components))) ceil(sqrt(length(other_components)))] ,0,'electrodes','on');
-    end
-    title(subject_list{s});
-    %saving figure
-    print([save_path '\' subject_list{s} '_other_bad_ICs_topos_manual'], '-dpng' ,'-r300');
-    
-    if auto_components.other_no_eye{1, s}==0
-        figure;title(subject_list{s}); text( 0.2,0.5, 'there are no "other" components auto selected')
-    elseif length(auto_components.other_no_eye{1, s}) == 1
-        figure;pop_topoplot(EEG ,0, auto_components.other_no_eye{1, s}, subject_list{s},[1 1] ,0,'electrodes','on');
-    else
-        pop_topoplot(EEG, 0, auto_components.other_no_eye{1, s} ,subject_list{s},[ceil(sqrt(length(auto_components.other_no_eye{1, s}))) ceil(sqrt(length(auto_components.other_no_eye{1, s})))] ,0,'electrodes','on');
-    end
-    title(subject_list{s});
-    %saving figure
-    print([save_path '\' subject_list{s} '_other_bad_ICs_topos_auto'], '-dpng' ,'-r300');
-    %combining both figures and deleting the separate ones
-    figure('units','normalized','outerposition',[0 0 1 1])
-    subplot(1,2,1);
-    imshow([save_path subject_list{s} '_other_bad_ICs_topos_manual.png']);
-    title('Manual')
-    subplot(1,2,2);
-    imshow([save_path subject_list{s} '_other_bad_ICs_topos_auto.png']);
-    title('Auto')
-    print([save_path subject_list{s} '_deleted_noise_components'], '-dpng' ,'-r300');
-    %deleting two original files
-    delete([save_path subject_list{s} '_other_bad_ICs_topos_manual.png'])
-    delete([save_path subject_list{s} '_other_bad_ICs_topos_auto.png'])
     close all
+    if strcmp(type, 'N')
+        pop_selectcomps(EEG, 1:EEG.nbchan);
+        prompt = 'Are there other components you would delete? If yes, input the numbers in [ ]. for example [1 2 10] ';
+        other_components = input(prompt);
+        
+        %% noise except eye part
+        %is going to create a topoplot and save it
+        if isempty(other_components)
+            figure;title(subject_list{s}); text( 0.2,0.5, 'there are no "other" components manually selected')
+            other_components = 0;
+        elseif length(other_components) == 1
+            figure;pop_topoplot(EEG ,0, other_components, subject_list{s},[1 1] ,0,'electrodes','on');
+        else
+            pop_topoplot(EEG, 0, other_components ,subject_list{s},[ceil(sqrt(length(other_components))) ceil(sqrt(length(other_components)))] ,0,'electrodes','on');
+        end
+        title(subject_list{s});
+        %saving figure
+        print([save_path '\' subject_list{s} '_other_bad_ICs_topos_manual'], '-dpng' ,'-r300');
+        
+        if auto_components.other_no_eye{1, s}==0
+            figure;title(subject_list{s}); text( 0.2,0.5, 'there are no "other" components auto selected')
+        elseif length(auto_components.other_no_eye{1, s}) == 1
+            figure;pop_topoplot(EEG ,0, auto_components.other_no_eye{1, s}, subject_list{s},[1 1] ,0,'electrodes','on');
+        else
+            pop_topoplot(EEG, 0, auto_components.other_no_eye{1, s} ,subject_list{s},[ceil(sqrt(length(auto_components.other_no_eye{1, s}))) ceil(sqrt(length(auto_components.other_no_eye{1, s})))] ,0,'electrodes','on');
+        end
+        title(subject_list{s});
+        %saving figure
+        print([save_path '\' subject_list{s} '_other_bad_ICs_topos_auto'], '-dpng' ,'-r300');
+        %combining both figures and deleting the separate ones
+        figure('units','normalized','outerposition',[0 0 1 1])
+        subplot(1,2,1);
+        imshow([save_path subject_list{s} '_other_bad_ICs_topos_manual.png']);
+        title('Manual')
+        subplot(1,2,2);
+        imshow([save_path subject_list{s} '_other_bad_ICs_topos_auto.png']);
+        title('Auto')
+        print([save_path subject_list{s} '_deleted_noise_components'], '-dpng' ,'-r300');
+        %deleting two original files
+        delete([save_path subject_list{s} '_other_bad_ICs_topos_manual.png'])
+        delete([save_path subject_list{s} '_other_bad_ICs_topos_auto.png'])
+        close all
+    end
     %% eye part
     if isempty(eye_components)
         figure;title(subject_list{s}); text( 0.2,0.5, 'there are no eye-components manually selected')
@@ -99,7 +109,7 @@ for s=1:length(subject_list)
     title(subject_list{s});
     %saving figure
     print([save_path '\' subject_list{s} '_Eye_ICs_auto'], '-dpng' ,'-r300');
-        %combining both figures and deleting the separate ones
+    %combining both figures and deleting the separate ones
     figure('units','normalized','outerposition',[0 0 1 1])
     subplot(1,2,1);
     imshow([save_path subject_list{s} '_Eye_ICs_manual.png']);
